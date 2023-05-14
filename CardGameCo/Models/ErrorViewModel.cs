ee9180a37583179981463c3419b592c8bf028a28
CommitDen <denielgimesi@gmail.com>
2023-05-14 17:22:05 +0200

    Init

---
+++ CardGameCo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CardGameCo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
