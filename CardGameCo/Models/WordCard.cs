de364901af73c4a86b9991e556868e01a6f06027
Gimesi Deniel <denielgimesi@gmail.com>
2023-05-19 13:23:08 +0200

    Update WordCard class to non language specific

---
+++ CardGameCo/Models/WordCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CardGameCo.Interfaces;
+
+namespace CardGameCo.Models
+{
+	public class WordCard : ICard
+	{
+        public WordCard(string language1, string language2, string word1, string word2)
+        {
+            Language1 = language1;
+            Language2 = language2;
+            Word1 = word1;
+            Word2 = word2;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 Id { get; set; }
+        public string Language1 { get; set; }
+        public string Language2 { get; set; }
+        public string Word1 { get; set; }
+        public string Word2 { get; set; }
+
+
+        public string Text1 { get => Word1; set => Word1 = value; }
+        public string Text2 { get => Word2; set => Word2 = value; }
+    }
+}
+
